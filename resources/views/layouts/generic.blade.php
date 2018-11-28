67f71ed8b06f1a01682522a39cd89e7489496da2
glcordon <glcordon@gmail.com>
2018-11-27 23:46:49 -0500

    update

--- resources/views/layouts/generic.blade.php
+++ resources/views/layouts/generic.blade.php
@@ -128,7 +128,9 @@
                    @yield('content') 
                 </div>
             
-              
-            </div>
+         </div>
+         <footer style="width:100%; min-height:20vh; background-color:black; margin-bottom:0; margin-top:30px; color:#fff">
+                hey
+        </footer>
 </body>
 </html>
