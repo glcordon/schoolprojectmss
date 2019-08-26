6f6aef828f59d7b720046fec58ec70b58d5e971a
glcordon <glcordon@gmail.com>
2019-08-25 21:44:28 -0400

    added youtube video embed

--- resources/views/courses/partials/course-card.blade.php
+++ resources/views/courses/partials/course-card.blade.php
@@ -1,6 +1,6 @@
 
     <div class="card mx-2" style="text-align:left;max-width: 22rem; min-width:18rem; margin-bottom:20px;">
-        <a href="/view-course/{{ $course->id }}">
+        <a href="/courses/show/{{ $course->id }}">
             <img class="card-img" src="{{ $course->course_video_thumb ?? asset('img/baseball-field.jpg') }}" alt="Card image cap" >
         </a>
         <div class="card-body">
@@ -8,6 +8,6 @@
             <h4 class="card-title">{{$course->course_title}}</h4>
         </div>  
         <div class="card-footer">
-            <a href="/view-course/{{ $course->id }}" class="card-link btn btn-primary pull-right col-md-12">View Course</a>
+            <a href="/course/show/{{ $course->id }}" class="card-link btn btn-primary pull-right col-md-12">View Course</a>
         </div>
     </div>  
