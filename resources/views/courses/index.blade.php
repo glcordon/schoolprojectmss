fb829232601249b56e7a23e877bfb0d2a98767d9
glcordon <glcordon@gmail.com>
2019-08-25 21:45:54 -0400

    added youtube video embed

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container" style="padding:50px 0px">
         <h2>Nick Duffy Golf</h2>
-        <div class="d-flex flex-wrap">
+        <div class="d-flex flex-wrap" id="courses">
             @foreach($courses as $course)
                 @include('courses.partials.course-card')
             @endforeach
