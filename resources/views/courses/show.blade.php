37fd96a607895e991604c279f12f9a7eda295048
glcordon <glcordon@gmail.com>
2019-08-25 21:51:02 -0400

    added youtube video embed

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -2,8 +2,12 @@
 
 @section('content')
     <div class="container" style="padding:50px 0px">
+        <h1>{{ $course->course_title }}</h1>
         <iframe src="{{ $course->course_intro_video }}"
         width="560" height="315" frameborder="0" allowfullscreen></iframe>
+        <div>
+            {{ $course->course_description }}
+        </div>
     </div>
 @endsection 
     
