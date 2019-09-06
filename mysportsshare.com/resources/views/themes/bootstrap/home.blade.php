35d4e8ff02f3334e72e3b43ce0daa336367ccaa9
glcordon <glcordon@gmail.com>
2019-09-06 18:04:25 -0400

    if session is null redirect to main site

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -6,10 +6,8 @@
 <div class="pt-5 mb-2 home-hero">
 	<div class="container">
 		<div class="row">
-			@dump(Session::get('tenant'))
 			@if(Session::get('tenant')->site_name)
-			{{ Session::get('tenant')->site_name }}
-
+			<h4> Welcome to {{ Session::get('tenant')->site_name }}</h4>
 			@endif
 			<div class="col-md-6 mt-5 pr-4">
 				<h1 class="text-left h6 mt-5">{{ theme('home_headline') }}</h1>
