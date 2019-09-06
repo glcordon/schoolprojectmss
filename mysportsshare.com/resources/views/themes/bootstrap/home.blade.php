3d82bbb7a8c096a614c6e8ce7bd075769439e7e6
glcordon <glcordon@gmail.com>
2019-09-06 17:57:21 -0400

    if session is null redirect to main site

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -52,7 +52,7 @@
 		<div class="row">
 			@foreach($sites as $site)
 			<div class="col-md-4 my-4 text-center">
-				<a href="{{ $site->site_slug }}.mysportsshare.com">
+				<a href="http://{{ $site->site_slug }}.mysportsshare.com">
 					<img src="{{ Storage::url($site->user_cover) }}">
 					<h4>{{ $site->site_name }}</h4>
 				</a>
