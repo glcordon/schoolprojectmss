9f92bd18d8546624d6dedcda6c7fa0e4bfd1e3d0
glcordon <glcordon@gmail.com>
2019-09-09 20:19:44 -0400

    Added site id by session

--- mysportsshare.com/resources/views/courses/index.blade.php
+++ mysportsshare.com/resources/views/courses/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container" style="padding:50px 0px">
-        <h2>Session::get('tenant')->site_name</h2>
+        <h2>{{ Session::get('tenant')->site_name }}</h2>
         <div class="d-flex flex-wrap" id="courses">
             @foreach($courses as $course)
                 @include('courses.partials.course-card')
