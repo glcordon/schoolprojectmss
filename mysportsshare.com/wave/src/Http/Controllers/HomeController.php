c77ec8e2f2b6d60312d90928873c4bd1a2ba36b0
glcordon <glcordon@gmail.com>
2019-09-10 23:35:29 -0400

    Updated background image

--- mysportsshare.com/wave/src/Http/Controllers/HomeController.php
+++ mysportsshare.com/wave/src/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends \App\Http\Controllers\Controller
         $sites = Site::where('id', Session::get('tenant')->id)->with('courses')->get();
         // dd(Session::get('tenant'));
         
-        dd($sites->all());
+        dd($sites->toArray());
     	if(setting('auth.dashboard_redirect', true) != "null"){
     		if(!\Auth::guest()){
     			return redirect('dashboard');
