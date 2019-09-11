f63bdb6765ea3c9e83c6f595cb5cd338fa3b9759
glcordon <glcordon@gmail.com>
2019-09-10 22:43:07 -0400

    Updated background image

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 <!-- HERO -->
-<div class="pt-5 mb-5 home-hero">
+<div class="pt-5 mb-5 home-hero" style="background-image:url({{ url(Session::get('tenant')->site_cover) ?? '' }});">
 	<div class="container">
 			@if(isset(Session::get('tenant')->site_name))
 				<h4 class="text-uppercase"> Welcome to {{ Session::get('tenant')->site_name }}</h4>
