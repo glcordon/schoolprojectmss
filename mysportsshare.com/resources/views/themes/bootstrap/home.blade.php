d3f9dce1cc1906a1931f625d0f005af4e0021f5f
glcordon <glcordon@gmail.com>
2019-09-12 19:13:49 -0400

    added eager loading to controller

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -50,7 +50,7 @@
 
 			<div class="d-flex flex-wrap col-12 align-items-stretch">
 				@if(isset(Session::get('tenant')->site_name))
-{{ dd($sites) }}
+{{ dd($sites->toArray()) }}
 							{{--  @foreach($sites->toArray()['courses'] as $course)
 								@include('courses.partials.course-card')
 							@endforeach  --}}
