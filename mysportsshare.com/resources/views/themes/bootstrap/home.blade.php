43f8096a58483b7909d6239a76a22cafe62d381d
glcordon <glcordon@gmail.com>
2019-09-12 19:25:18 -0400

    updated foreach loop for courses on homepage

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -50,7 +50,7 @@
 
 			<div class="d-flex flex-wrap col-12 align-items-stretch">
 				@if(isset(Session::get('tenant')->site_name))
-{{ dump($sites->toArray()) }}
+{{ dump($sites) }}
 							@foreach($sites->courses->toArray() as $course)
 								@include('courses.partials.course-card')
 							@endforeach 
