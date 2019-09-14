c95a067643e0cba4f61db206fe4ab38ac8b26fae
glcordon <glcordon@gmail.com>
2019-09-14 08:19:22 -0400

    removed unneccessary files

--- resources/views/themes/bootstrap/partials/impersonation-bar.blade.php
+++
@@ -1,6 +0,0 @@
-<div id="impersonation-bar">
-	<div class="container">
-		<p>You are currently impersonating {{ Auth::user()->email }}</p>
-		<a href="{{ route('impersonate.leave') }}"><span class="mr-3" uk-icon="sign-out"></span> Leave impersonation</a>
-	</div>
-</div>
