dce7fce4514a69d8b1f3d0ce3d47ccb5aeb097e8
glcordon <glcordon@gmail.com>
2019-09-16 22:14:57 -0400

    Added relationship between courses and sites in the models

--- mysportsshare.com/app/Http/Controllers/CoursesController.php
+++ mysportsshare.com/app/Http/Controllers/CoursesController.php
@@ -43,7 +43,7 @@
             // } else {
             //     $courses = Course::all();
             // }
-            $course = Course::where('course_title', '<>', '')->get();
+            $course = Course::where('course_title', '<>', '')->courses()->get();
             $siteData = Site::find(1);
             if(Session::get('tenant') !== "home")
             {
