ad4d907defbdc81eebdaf34048d1567a6a55d599
glcordon <glcordon@gmail.com>
2019-09-16 23:14:02 -0400

    now testing the questions and answer array

--- mysportsshare.com/app/Quiz.php
+++ mysportsshare.com/app/Quiz.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Quiz extends Model
 {
     protected $guarded = [];
-
+    protected $table = 'Quiz';
     public function questions()
     {
         return $this->hasMany(Questions::class);
