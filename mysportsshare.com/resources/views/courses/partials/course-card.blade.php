9d0030e8153a82ccf8384e23d13e45a899827567
glcordon <glcordon@gmail.com>
2019-09-16 21:59:51 -0400

    Downgrading voyager

--- mysportsshare.com/resources/views/courses/partials/course-card.blade.php
+++ mysportsshare.com/resources/views/courses/partials/course-card.blade.php
@@ -1,6 +1,6 @@
 
     <div class="card mx-2" style="text-align:left;max-width: 22rem; min-width:18rem; margin-bottom:20px;">
-@dd($course)
+@dump($course)
         <a href="/courses/show/{{ $course['id'] }}">
             @if($course['course_image'])
             <img class="card-img" src="{{ Storage::url($course['course_image'])  ?? asset('img/baseball-field.jpg') }}" alt="Card image cap" >
@@ -14,7 +14,7 @@
         </div>  
         <div class="card-footer">
             <a href="/courses/show/{{ $course['id'] }}" class="card-link btn btn-primary pull-right col-md-12">View Course</a>
-            @can('edit', \App\Course::class)
+            @can('edit', $courses)
                 <div class="d-flex justify-content-between col-12 px-0 py-1">
                     <div><a href="/courses/{{ $course['id'] }}/create" class="btn btn-warning">Edit</a></div>
                     <div><a href="/courses/{{ $course['id'] }}/delete" class="btn btn-danger"  onclick="return confirm('Are you sure?')">Delete</a></div>
