7757858d766004f92632a337d651b12cd8b3191b
glcordon <glcordon@gmail.com>
2019-09-23 00:10:25 -0400

    testing jq

--- mysportsshare.com/app/Http/Controllers/CoursesController.php
+++ mysportsshare.com/app/Http/Controllers/CoursesController.php
@@ -33,6 +33,14 @@
 
         public function addQuestion(Request $request)
         {
+            $course = Course::find($request->id);
+            $quizTitle = $course->course_name . ' quiz';
+            $quiz = \App\Quiz::firstOrNew(['course_id' => $request->id], ['quiz_name' => $quizTitle]);
+            foreach($request->question as $question)
+            {
+               $quiz->question()->save($question); 
+            }
+            return $quiz->question;
             return $request->all();
         }
 
