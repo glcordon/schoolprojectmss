5c5b1c112bebf2e05f2fc37723f4442b6d9ff104
glcordon <glcordon@gmail.com>
2019-09-23 20:05:46 -0400

    Added relationships

--- mysportsshare.com/resources/views/courses/show.blade.php
+++ mysportsshare.com/resources/views/courses/show.blade.php
@@ -65,7 +65,7 @@
         @endif
         <div class="tab-pane fade" id="quiz" role="tabpanel" aria-labelledby="quiz-tab">
         <h2>Quiz</h2>
-        @dd($course->questions)
+        @dd($course)
           @foreach($course->questions as $quesiton)
           {{ $question->question_text }}
           @endforeach
