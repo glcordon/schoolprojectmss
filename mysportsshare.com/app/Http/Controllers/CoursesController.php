a690b800496e23b11841daff1214537a29d5211c
glcordon <glcordon@gmail.com>
2019-09-24 22:39:35 -0400

    Added delete method

--- mysportsshare.com/app/Http/Controllers/CoursesController.php
+++ mysportsshare.com/app/Http/Controllers/CoursesController.php
@@ -62,6 +62,7 @@
         public function updateQuestion(Request $request)
         {
             $question = \App\Questions::where('id',$request->id)->with('answers')->get();
+            return $request->all();
             return $question;
         }
         public function deleteQuestion(Request $request)
