663f3c08a6c25247738ad306c7c6f709fcd7073e
glcordon <glcordon@gmail.com>
2019-10-01 21:24:23 -0400

    Added card

--- mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
+++ mysportsshare.com/resources/views/themes/bootstrap/home.blade.php
@@ -81,7 +81,7 @@
 					@foreach($sites as $site)
 					
 					<div class="card bg-dark text-white col-4">
-						<img src="{{ Storage::url($site->user_cover) }}" class="col-12"> 
+						<img src="{{ Storage::url($site->user_cover) }}"> 
 						<a href="http://{{ $site->site_slug }}.mysportsshare.com">
 							<div class="card-img-overlay">
 								<div class="card-text border-0 bg-semitransparent text-center text-white">
