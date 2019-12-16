095c886d2240fed3c2a951db1e6f9046d4ea76a9
glcordon <glcordon@gmail.com>
2019-12-15 21:41:34 -0500

    testing

---
+++ mysportsshare.com/resources/views/layouts/app1.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-icon-60x60.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-icon-76x76.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-icon-114x114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-icon-120x120.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-icon-144x144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-icon-152x152.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-icon-180x180.png">
+        <link rel="icon" type="image/png" sizes="192x192"  href="/icon/android-icon-192x192.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/icon/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon/favicon-16x16.png">
+        <link rel="manifest" href="/icon/manifest.json">
+        <meta name="msapplication-TileColor" content="#ffffff">
+        <meta name="msapplication-TileImage" content="/icon/ms-icon-144x144.png">
+        <meta name="theme-color" content="#ffffff">
+
+        <title>MSS</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 44px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 12px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+
+            .m-b-xl {
+                margin-bottom: 60px;
+            }
+            .hero{
+                height:70vh;
+                background-size:cover;
+                color:#fff;
+                background: url({{ asset('img/baseball-field.jpg') }});
+            }
+            .top-right.links a{color:white}
+            .card-img {
+                object-fit: none; /* Do not scale the image */
+                object-position: center; /* Center the image within the element */
+                width: 100%;
+                max-height: 150px;
+                margin-bottom: 1rem;
+              }
+        </style>
+    </head>
+    <body>
+        <a class="navbar-brand" href="{{ url('/') }}"><img src="{{asset('img/mss-logo.png')}}" alt=""> <a>        
+        <div class="flex-center position-ref hero">
+       
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+            <div class="content">
+                
+            
+              @yield('content')
+            </div>
+        </div>
+    <script>
+            $(function() {
+              $('input[name="datetimes"]').daterangepicker({
+                timePicker: true,
+                startDate: moment().startOf('hour'),
+                endDate: moment().startOf('hour').add(32, 'hour'),
+                locale: {
+                  format: 'M/DD hh:mm A'
+                }
+              });
+            });
+            </script>
+</body>
+</html>
